dc05a9b02bb4faec89e47b58a2a923541b652862
Caroline Ferraz <projetos.crrz@gmail.com>
2022-10-26 12:27:27 -0300

    Feat: Adding pagination and limiting pokemon display to first generation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,38 @@
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.img}" alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreBtn = document.getElementById('loadMoreBtn');
+let offset = 0;
+let limit = 12;
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>
+            `
+                <li class="pokemon ${pokemon.type}">
+                    <div class="nameAndNumber">
+                        <span class="name">${pokemon.name}</span>
+                        <span class="number">#${pokemon.number}</span>
+                    </div>
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                        </ol>
+                        <img src="${pokemon.img}" alt="${pokemon.name}">
+                    </div>
+                </li>
+        `
+        ).join('');
+
+        pokemonList.innerHTML += newHtml;
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonItems(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+loadMoreBtn.addEventListener('click', () => {
+    offset += limit;
+    if(offset == 144) {
+        limit = 7;
+        loadMoreBtn.parentElement.removeChild(loadMoreBtn);
+    } 
+    loadPokemonItems(offset, limit)
 })
